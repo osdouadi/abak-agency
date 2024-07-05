822d3a51e351ba5450a732579fe4585571b9e3ff
osdouadi <osdouadi@gmail.com>
2024-07-05 17:05:00 +0100

    Initial

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["uploadthing.com", "utfs.io", "img.clerk.com", "subdomain"],
+  },
+  reactStrictMode: false,
+};
 
 export default nextConfig;
